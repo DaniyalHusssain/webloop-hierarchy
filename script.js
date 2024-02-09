52bc35136c730aae785160bc1bec6a875eb8450b
DaniyalHusssain <danhussain29@gmail.com>
2024-02-09 16:05:54 +0500

    updated

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let info = document.querySelectorAll(".container");
 const data = [
   {
     level: 1,
@@ -39,7 +38,7 @@ const data = [
               },
               {
                 name: "Asad",
-                Skill: "Nodejs/php",
+                skill: "Nodejs/php",
               },
             ],
           },
@@ -60,89 +59,77 @@ const data = [
     },
   },
 ];
-
-// console.log(data);
 for (let i = 0; i < data.length; i++) {
-  const element = data[i];
-  // console.log(element.hir.managers);
-  // console.log(element.hir.subhir1.types[0].developers[1]);
-  // console.log(element.hir.subhir1.types[0].developers[0].skill);
-  // console.log(element.hir.subhir1.types[0].developers[1].skill);
-  // console.log(element.hir.subhir1.types[1].developers[0].skill);
-  // console.log(element.hir.subhir1.types[1].developers[1].skill);
-  // console.log(element.hir.subhir2.interns[0].name);
-  // console.log(element.hir.subhir2.interns[1].name);
-  // console.log(element.hir.subhir2.interns[0].name);
-  // console.log(element.hir.subhir2.interns[1].name);
-  // console.log(element.hir.subhir2.interns[0].name);
-  // Assuming you have only one button with class "CEO"
-  let btn = document.querySelectorAll(".CEO");
-  btn[0].addEventListener("click", () => {
-    // console.log(element);
-    console.log(data[i].CEO);
-    element.innerhtml += `
-    <div class=".CEO-info">
+  let btnCEO = document.querySelector(".CEO");
+  btnCEO.addEventListener("click", () => {
+    let container = document.querySelector(".container");
+    container.innerHTML += `
+    <div class="CEO-info">
       <h1>${data[i].CEO.name}</h1>
       <h2>${data[i].CEO.age}</h2>
     </div>`;
   });
-  let btn2 = document.querySelectorAll(".manager");
-  btn2[0].addEventListener("click", () => {
-    // console.log(element);
-    console.log(data[i].hir.managers);
-    element.innerhtml += `
-    <div class=".manager-info">
-    <h1>${data[i].hir.managers.name}</h1>
-    <h2>${data[i].hir.managers.age}</h2>`;
+  let btnManager = document.querySelector(".manager");
+  btnManager.addEventListener("click", () => {
+    let container = document.querySelector(".container");
+    container.innerHTML += `
+    <div class="manager-info">
+      <h1>${data[i].hir.managers.name}</h1>
+      <h2>${data[i].hir.managers.age}</h2>
+    </div>`;
   });
-  let btn3 = document.querySelectorAll(".info");
-  btn3[0].addEventListener("click", () => {
-    // console.log(element);
-    console.log(data[i].hir.subhir1.types[0].name);
-    console.log(data[i].hir.subhir1.types[1].name);
-    element.innerhtml += `
-    <div class=".info-info">
-    <h1>${data[i].hir.subhir1.types[0].name}</h1>
-    <h2>${data[i].hir.subhir1.types[1].name}</h2>`;
+
+  let btnInfo = document.querySelector(".info");
+  btnInfo.addEventListener("click", () => {
+    let container = document.querySelector(".container");
+    container.innerHTML += `
+    <div class="info-info">
+      <h1>${data[i].hir.subhir1.types[0].name}</h1>
+      <h2>${data[i].hir.subhir1.types[1].name}</h2>
+    </div>`;
   });
-  let btn4 = document.querySelectorAll(".frontend-info");
-  btn4[0].addEventListener("click", () => {
-    // console.log(element);
-    console.log(data[i].hir.subhir1.types[0].developers[0].skill);
-    console.log(data[i].hir.subhir1.types[0].developers[1].skill);
-    element.innerhtml += `
-    <div class=".frontend-info">
-    <h1>${data[i].hir.subhir1.types[0].developers[0].skill}</h1>
-    <h2>${data[i].hir.subhir1.types[0].developers[1].skill}</h2>`;
+
+  let btnFrontend = document.querySelector(".frontend-info");
+  btnFrontend.addEventListener("click", () => {
+    let container = document.querySelector(".container");
+    container.innerHTML += `
+    <div class="frontend-info">
+      <h1>${data[i].hir.subhir1.types[0].developers[0].name}</h1>
+      <h2>${data[i].hir.subhir1.types[0].developers[0].skill}</h2>
+
+       <h1>${data[i].hir.subhir1.types[0].developers[1].name}</h1>
+        <h2>${data[i].hir.subhir1.types[0].developers[1].skill}</h2>
+
+    </div>`;
   });
-  let btn5 = document.querySelectorAll(".backend-info");
-  btn5[0].addEventListener("click", () => {
-    // console.log(element);
-    console.log(data[i].hir.subhir1.types[1].developers[0].skill);
-    console.log(data[i].hir.subhir1.types[1].developers[1].skill);
-    element.innerhtml += `
-    <div class=".backend-info">
-    <h1>${data[i].hir.subhir1.types[1].developers[0].skill}</h1>
-    <h2>${data[i].hir.subhir1.types[1].developers[1].skill}</h2>`;
+
+  let btnBackend = document.querySelector(".backend-info");
+  btnBackend.addEventListener("click", () => {
+    let container = document.querySelector(".container");
+    container.innerHTML += `
+    <div class="backend-info">
+      <h1>${data[i].hir.subhir1.types[1].developers[0].name}</h1>
+      <h2>${data[i].hir.subhir1.types[1].developers[0].skill}</h2>
+      <h1>${data[i].hir.subhir1.types[1].developers[1].name}</h1>
+      <h2>${data[i].hir.subhir1.types[1].developers[1].skill}</h2>
+    </div>`;
   });
-  let btn6 = document.querySelectorAll(".Frontend-intern");
-  btn6[0].addEventListener("click", () => {
-    // console.log(element);
-    console.log(data[i].hir.subhir2.interns[0].name);
-    console.log(data[i].hir.subhir2.interns[1].name);
-    element.innerhtml += `
-    <div class=".intern-info">
-    <h1>${data[i].hir.subhir2.interns[0].name}</h1>
-   `;
+
+  let btnFrontendIntern = document.querySelector(".Frontend-intern");
+  btnFrontendIntern.addEventListener("click", () => {
+    let container = document.querySelector(".container");
+    container.innerHTML += `
+    <div class="intern-info">
+      <h1>${data[i].hir.subhir2.interns[0].name}</h1>
+    </div>`;
   });
-  let btn7 = document.querySelectorAll(".Backend-intern");
-  btn7[0].addEventListener("click", () => {
-    // console.log(element);
-    console.log(data[i].hir.subhir2.interns[0].name);
-    console.log(data[i].hir.subhir2.interns[1].name);
-    element.innerhtml += `
-    <div class=".intern-info">
-    <h1>${data[i].hir.subhir2.interns[1].name}</h1>
-   `;
+
+  let btnBackendIntern = document.querySelector(".Backend-intern");
+  btnBackendIntern.addEventListener("click", () => {
+    let container = document.querySelector(".container");
+    container.innerHTML += `
+    <div class="intern-info">
+      <h1>${data[i].hir.subhir2.interns[1].name}</h1>
+    </div>`;
   });
 }
