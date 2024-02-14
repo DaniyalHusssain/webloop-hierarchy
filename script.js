db3c05ea61695dbf62842328dbfd23bcad019163
DaniyalHusssain <danhussain29@gmail.com>
2024-02-14 12:44:58 +0500

    changes made

--- script.js
+++ script.js
@@ -1,281 +1,194 @@
-const data = [
-  {
-    level: 1,
-    name: "CEO",
-    CEO: {
-      name: "Sheraz",
+const data = {
+  level: 1,
+  name: "CEO",
+  CEO: {
+    name: "Sheraz",
+    age: 27,
+    img: "/assesst/sheraz.png",
+    designation: "CEO",
+  },
+  hir: {
+    level: 2,
+    name: "Manager",
+    managers: {
+      name: "Saleem",
       age: 27,
-      img: "/assesst/sheraz.png",
-      designation: "CEO",
+      img: "/assesst/saleem.png",
+      designation: "HR Manager",
     },
-    hir: {
-      level: 2,
-      name: "Manager",
-      managers: {
-        name: "Saleem",
-        age: 27,
-        img: "/assesst/saleem.png",
-        designation: "HR Manager",
-      },
-      subhir1: {
-        level: 3,
-        types: [
-          {
-            name: "front-end",
-            developers: [
-              {
-                name: "Saqib",
-                skill: "react.js",
-                img: "/assesst/saqib.png",
-                designation: "Front-end Developer",
-              },
-              {
-                name: "ALiyan",
-                skill: "html,js",
-                img: "/assesst/saqib.png",
-                designation: "Front-end Developer",
-              },
-              {
-                name: "Shakir",
-                skill: "react.js",
-                img: "/assesst/saqib.png",
-                designation: "Front-end Developer",
-              },
-            ],
-          },
-          {
-            name: "backend",
-            developers: [
-              {
-                name: "Sheraz",
-                skill: "nodejs/php/blockchain",
-                img: "/assesst/sheraz.png",
-                designation: "Back-end Developer",
-              },
-              {
-                name: "Asad",
-                skill: "Nodejs/php",
-                img: "/assesst/Asad.png",
-                designation: "Back-end Developer",
-              },
-            ],
-          },
-        ],
-      },
-      subhir2: {
-        level: 4,
-        name: "interns",
-        interns: [
-          {
-            name: "Mansoor",
-            img: "/assesst/saqib.png",
-            designation: "Front-end intern",
-          },
-          {
-            name: "Ali Abbass",
-            img: "/assesst/saqib.png",
-            designation: "Front-end intern",
-          },
-          {
-            name: "Kashif",
-            img: "/assesst/saqib.png",
-            designation: "Back-end intern",
-          },
-          {
-            name: "Maqbool",
-            img: "/assesst/saqib.png",
-            designation: "Back-end intern",
-          },
-        ],
-      },
+    subhir1: {
+      level: 3,
+      types: [
+        {
+          name: "front-end",
+          developers: [
+            {
+              name: "Saqib",
+              skill: "react.js",
+              img: "/assesst/saqib.png",
+              designation: "Front-end Developer",
+            },
+            {
+              name: "ALiyan",
+              skill: "html,js",
+              img: "/assesst/Aliyan.png",
+              designation: "Front-end Developer",
+            },
+            {
+              name: "Shakir",
+              skill: "react.js",
+              img: "/assesst/shakir.png",
+              designation: "Front-end Developer",
+            },
+          ],
+        },
+        {
+          name: "backend",
+          developers: [
+            {
+              name: "Sheraz",
+              skill: "nodejs/php/blockchain",
+              img: "/assesst/sheraz.png",
+              designation: "Back-end Developer",
+            },
+            {
+              name: "Asad",
+              skill: "Nodejs/php",
+              img: "/assesst/Asad.png",
+              designation: "Back-end Developer",
+            },
+            {
+              name: "Kashif",
+              img: "/assesst/kashif.png",
+              designation: "Back-end developer",
+            },
+            {
+              name: "Nasir",
+              img: "/assesst/Nasir.png",
+              designation: "Back-end developer",
+            },
+          ],
+        },
+      ],
+    },
+    subhir2: {
+      level: 4,
+      name: "interns",
+      interns: [
+        {
+          name: "Shehzad",
+          img: "/assesst/shahzad.png",
+          designation: "Front-end intern",
+        },
+        {
+          name: "Ali Abbass",
+          img: "/assesst/saqib.png",
+          designation: "Front-end intern",
+        },
+      ],
     },
   },
-];
+};
 
-document.addEventListener("DOMContentLoaded", function () {
-  let container = document.querySelectorAll(".Parentnode");
+const Ceo = document.querySelectorAll(".CeoDiv");
 
-  for (let i = 0; i < container.length; i++) {
-    container[i].innerHTML += `
-        <div class="Sheraz">
-            <div class="CEO-info">
-                <img src="${data[i].CEO.img}">
-                <div class="CEO-text">
-                    <h1> Name : ${data[i].CEO.name}</h1>
-                    <h2>Age : ${data[i].CEO.age}</h2>
-                    <h3>Title: ${data[i].CEO.designation}</h3>
-                </div>
-            </div>
-        </div>`;
-
-    let btnCEO = document.querySelector(".CEO");
-    btnCEO.addEventListener("click", () => {
-      btnCEO.style.display = "none";
-      let ceo = document.querySelectorAll(".Sheraz");
-      ceo[i].style.display = "block";
-    });
+const ceobtn = document.getElementById("Ceobtn");
+// console.log(data.CEO);
+ceobtn.addEventListener("click", () => {
+  const CEO = data.CEO;
+  ceobtn.style.display = "none";
+  for (let i = 0; i < Ceo.length; i++) {
+    console.log(CEO.age);
+    Ceo[i].innerHTML += `
+    <div class="Ceo dp3">
+    <img src="${CEO.img}"  class="img ceo_img">
+     <div id="CeoInfo" class= "info dvlprsDp">
+      <h2 class= "ceoH2"> Name :${CEO.name} <br>
+      Age :${CEO.age}</h2>
+    </div>
+    </div>
+          `;
   }
+});
+const managr = document.querySelectorAll(".HrDiv");
 
-  let childnode = document.querySelectorAll(".Childnode");
+const hrbtn = document.getElementById("hrbtn");
 
-  for (let i = 0; i < childnode.length; i++) {
-    childnode[i].innerHTML += `
-        <div class="Saleem">
-            <div class="Manager-info">
-                <img src ="${data[i].hir.managers.img}">
-                <div class="Manager-text">
-                    <h1> Name: ${data[i].hir.managers.name}</h1>
-                    <h2> Age :${data[i].hir.managers.age}</h2>
-                    <h3> Title :${data[i].hir.managers.designation}</h3>
-                </div>
-            </div>
-        </div>`;
-    let btnManager = document.querySelector(".manager");
-
-    btnManager.addEventListener("click", () => {
-      btnManager.style.display = "none";
-      let manager = document.querySelectorAll(".Saleem");
-      manager[i].style.display = "block";
-    });
+hrbtn.addEventListener("click", () => {
+  hrbtn.style.display = "none";
+  const HR = data.hir.managers;
+  for (let i = 0; i < managr.length; i++) {
+    console.log(HR.age);
+    managr[i].innerHTML += `
+    <div class="Hr dp3">
+    <img src="${HR.img}"  class="img hr_img">
+     <div id="HrInfo" class= "info dvlprsDp">
+      <h2 class= "hrH2"> Name :${HR.name} <br>
+      Age :${HR.age}</h2>
+    </div>
+    </div>
+          `;
   }
+});
 
-  // let info = document.querySelectorAll(".info");
-  // for (let i = 0; i < info.length; i++) {
-  //   info[i].innerHTML += `
-  //       <div class="info-info">
-  //           <h1>${data[i].hir.subhir1.types[0].name}</h1>
-  //           <h2>${data[i].hir.subhir1.types[1].name}</h2>
-  //       </div>`;
-  //   let btnInfo = document.querySelector(".info");
-  //   btnInfo.addEventListener("click", () => {
-  //     btnInfo.style.display = "none";
-  //     let info = document.querySelectorAll(".infodev");
-  //     infodev[i].style.display = "block";
-  //   });
-  // }
-
-  let frontend = document.querySelectorAll(".dev-info");
-
-  for (let i = 0; i < frontend.length; i++) {
-    frontend[i].innerHTML += `
-        <div class="frontend-dev">
-            <div class ="fdeveloper">
-                <div class ="Saqib-info">
-                    <img src = "${data[i].hir.subhir1.types[0].developers[0].img}">
-                    <div class ="Saqib-text">
-                        <h1>${data[i].hir.subhir1.types[0].developers[0].name}</h1>
-                        <h2>${data[i].hir.subhir1.types[0].developers[0].skill}</h2>
-                    </div>
-                </div>
-                <div class ="Aliyan-info">
-                    <img src = "${data[i].hir.subhir1.types[0].developers[1].img}">
-                    <div class ="Aliyan-text">
-                        <h1>${data[i].hir.subhir1.types[0].developers[1].name}</h1>
-                        <h2>${data[i].hir.subhir1.types[0].developers[1].skill}</h2>
-                    </div>
-                </div>
-                <div class ="Shakir-info">
-                    <img src = "${data[i].hir.subhir1.types[0].developers[2].img}">
-                    <div class ="Shakir-text">
-                        <h1>${data[i].hir.subhir1.types[0].developers[2].name}</h1>
-                        <h2>${data[i].hir.subhir1.types[0].developers[2].skill}</h2>
-                    </div>
-                </div>
-            </div>
-        </div>`;
-
-    let btnFrontend = document.querySelector(".frontend-info");
-    btnFrontend.addEventListener("click", () => {
-      console.log("Front");
-      btnFrontend.style.display = "none";
-      let frontend = document.querySelectorAll(".frontend-dev");
-      frontend[i].style.display = "block";
-    });
-  }
-
-  let backend = document.querySelectorAll(".dev-info");
-
-  for (let i = 0; i < backend.length; i++) {
-    backend[i].innerHTML += `
-        <div class="backend-dev">
-            <div class ="bdeveloper">
-                <div class ="Sheraz-info">
-                    <img src = "${data[i].hir.subhir1.types[1].developers[0].img}">
-                    <div class ="Sheraz-text">
-                        <h1>${data[i].hir.subhir1.types[1].developers[0].name}</h1>
-                        <h2>${data[i].hir.subhir1.types[1].developers[0].skill}</h2>
-                    </div>
-                </div>
-                <div class="Asad-info">
-                    <img src = "${data[i].hir.subhir1.types[1].developers[1].img}">
-                    <div class ="Asad-text">
-                        <h1>${data[i].hir.subhir1.types[1].developers[1].name}</h1>
-                        <h2>${data[i].hir.subhir1.types[1].developers[1].skill}</h2>
-                    </div>
-                </div>
-            </div>
-        </div>`;
-
-    let btnBackend = document.querySelector(".backend-info");
-    btnBackend.addEventListener("click", () => {
-      btnBackend.style.display = "none";
-      let backend = document.querySelectorAll(".backend-dev");
-      backend[i].style.display = "block";
-    });
-  }
-  let intern = document.querySelectorAll(".interns-info");
-  for (let i = 0; i < intern.length; i++) {
-    intern[i].innerHTML += `
-        <div class="intern-info">
-        <div class ="frontend-intern">
-        <div class ="Mansoor-info">
-        <img src = "${data[i].hir.subhir2.interns[0].img}">
-        <div class ="Mansoor-text">
-            <h1>${data[i].hir.subhir2.interns[0].name}</h1>
-            <h2>${data[i].hir.subhir2.interns[0].designation}</h2>
-             </div>
-            </div>
-            <div class ="Ali-info">
-            <img src = "${data[i].hir.subhir2.interns[1].img}">
-            <div class ="Ali-text">
-            <h1>${data[i].hir.subhir2.interns[1].name}</h1>
-            <h2>${data[i].hir.subhir2.interns[1].designation}</h2>
-            </div>
-            </div>
-        </div>
-        </div>`;
-    let btnIntern = document.querySelector(".Frontend-intern");
-    btnIntern.addEventListener("click", () => {
-      btnIntern.style.display = "none";
-      let intern = document.querySelectorAll(".intern-info");
-      intern[i].style.display = "block";
-    });
-  }
-  let backintern = document.querySelectorAll(".interns-info");
-  for (let i = 0; i < backintern.length; i++) {
-    backintern[i].innerHTML += `
-     <div class="b-intern-info">
-     <div class ="backend-intern">
-     <div class ="Kashif-info">
-     <img src = "${data[i].hir.subhir2.interns[2].img}">
-     <div class ="Kashif-text">
-     <h1>${data[i].hir.subhir2.interns[2].name}</h1>
-     <h2>${data[i].hir.subhir2.interns[2].designation}</h2>
-   </div>
-    <div class ="Maqbul-info">
-    <img src = "${data[i].hir.subhir2.interns[3].img}">
-    <div class ="Maqbul-text">
-    <h1>${data[i].hir.subhir2.interns[3].name}</h1>
-    <h2>${data[i].hir.subhir2.interns[3].designation}</h2>
+const frnt_dev = document.getElementById("F_Dvlpr");
+const Frnt = document.getElementById("FrontDevbtn");
+Frnt.addEventListener("click", () => {
+  Frnt.style.display = "none";
+  const FrntDvlpr = data.hir.subhir1.types[0].developers;
+  for (let i = 0; i < FrntDvlpr.length; i++) {
+    console.log(FrntDvlpr[i].name);
+    frnt_dev.innerHTML += `
+        <div class="FDev dp3">
+        <img src="${FrntDvlpr[i].img}"  class="img frnt_img">
+         <div id="FrDevInfo" class= "info dvlprsDp">
+          <h2 class= "frntH2"> Name :${FrntDvlpr[i].name} <br>
+          Skills :${FrntDvlpr[i].skill}</h2>
         </div>
         </div>
+       <div class="h_line nonelines" ></div>
+        `;
+  }
+});
+const interns = document.getElementById("intrns");
+const InternBtn = document.getElementById("FrontInters");
+
+InternBtn.addEventListener("click", () => {
+  InternBtn.style.display = "none";
+  const Interns = data.hir.subhir2.interns;
+  for (let i = 0; i < Interns.length; i++) {
+    console.log(Interns[i].name);
+    interns.innerHTML += `
+        <div class="intns dp3">
+        <img src="${Interns[i].img}"  class="img intns_img">
+         <div id="intnsInfo" class= "info dvlprsDp">
+          <h2 class= "intnsH2"> Name :${Interns[i].name} <br>
+          Skills :${Interns[i].designation}</h2>
         </div>
-    `;
-    let btnBackintern = document.querySelector(".Backend-intern");
-    btnBackintern.addEventListener("click", () => {
-      btnBackintern.style.display = "none";
-      let backintern = document.querySelectorAll(".b-intern-info");
-      backintern[i].style.display = "block";
-    });
+        </div>
+         <div class="h_line nonelines" ></div>
+        `;
+  }
+});
+
+const back_dev = document.getElementById("B_Dvlpr");
+
+const Back = document.getElementById("bckDev");
+
+Back.addEventListener("click", () => {
+  Back.style.display = "none";
+  const BackDvlpr = data.hir.subhir1.types[1].developers;
+  for (let i = 0; i < BackDvlpr.length; i++) {
+    console.log(BackDvlpr[i].name);
+    back_dev.innerHTML += `
+        <div class="BDev dp3">
+        <img src="${BackDvlpr[i].img}"  class="img back_img">
+         <div id="BackDevInfo" class= "info dvlprsDp">
+          <h2 class= "backH2"> Name :${BackDvlpr[i].name} <br>
+          Skills :${BackDvlpr[i].designation}</h2>
+        </div>
+        </div>
+         <div class="h_line nonelines" ></div>
+        `;
   }
 });
